a18feec13423fd196e4a3b0c387feeeba198d1d9
Exanimates <exanimatesr@gmail.com>
2018-09-29 22:14:32 +0700

    I made the basic game logic: check of a victory, a course of the player, removal of event click at pressing a button, clearing of a game field

--- JavaScript/GameLogic.js
+++ JavaScript/GameLogic.js
@@ -1,15 +1,56 @@
 class GameLogic{
 
 	constructor(){
-		this.ceil = document.getElementsByClassName("game-item");
-		this.reset = document.getElementById("reset-game");
+		this.board = document.getElementById("board");
 		this.message = document.getElementById("message");
 		this.player = "X";
 		this.stepCount = 0;
 	}
 
 	currentStep() {
+		if (!this.textContent) {
+			this.innerText = gameLogic.player;
+			gameLogic.checkWin();
+		}
+	}
 
+	checkWin(){
+		var flag;
+		var count = gameLogic.board.getElementsByTagName("tr").length;
+		for(var i = 0; i < count; i++){
+			var winRow = true,
+				winColumn = true,
+				winLeftTop = true,
+				winLeftBottom = true;
+
+			for(var k = 0; k < count; k++){
+				if(gameLogic.board.rows[i].cells[k].innerHTML !== gameLogic.player) winRow = false;
+				if(gameLogic.board.rows[k].cells[i].innerHTML !== gameLogic.player) winColumn = false;
+				if(gameLogic.board.rows[k].cells[k].innerHTML !== gameLogic.player) winLeftTop = false;
+				if(gameLogic.board.rows[count-1-k].cells[k].innerHTML !== gameLogic.player) winLeftBottom = false;
+			}
+
+			if(winRow || winColumn || winLeftTop || winLeftBottom){
+				flag = true;
+				gameLogic.removeEvent();
+				break;
+			}
+
+		}
+	}
+
+	removeEvent(){
+		var elements = gameLogic.board.getElementsByTagName("td");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].removeEventListener('click',gameLogic.currentStep);
+		}
+	}
+
+	removeBoard(){
+		var elements = gameLogic.board.getElementsByTagName("td");
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].innerHTML = '';
+		}
 	}
 
 }
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,3 +3,5 @@ articles = document.getElementsByTagName('td');
 for (var i = 0; i < articles.length; i++) {
 	articles[i].addEventListener('click',gameLogic.currentStep);
 }
+const resetGame = document.getElementById("reset-game");
+resetGame.addEventListener('click',gameLogic.removeBoard)
