218cc1075cff2a1204d27f2064606787531371ff
Exanimates <exanimatesr@gmail.com>
2018-09-30 16:24:03 +0700

    Added  "defense" logic of the computer's steps

--- JavaScript/GameLogic.js
+++ JavaScript/GameLogic.js
@@ -3,19 +3,158 @@ class GameLogic{
 	constructor(){
 		this.board = document.getElementById("board");
 		this.message = document.getElementById("message");
-		this.player = "X";
+		this.player = "x";
 		this.stepCount = 0;
 	}
 
 	currentStep() {
 		if (!this.textContent) {
-			this.innerText = gameLogic.player;
-			gameLogic.checkWin();
+
+			if (gameLogic.player === "x")
+				this.innerText = gameLogic.player;
+			this.classList.add(gameLogic.player);
+			if( typeof gameLogic.checkWin() === "string" ){
+				return;
+			}
+			gameLogic.changePlayer();
+			if (gameLogic.player === "o"){
+				gameLogic.computerStep();
+				if( typeof gameLogic.checkWin() === "string" ){
+					return;
+				}
+				gameLogic.changePlayer();
+			}
 		}
+	}
+
+	computerStep(){
+		var count = gameLogic.board.getElementsByTagName("tr").length;
+		if (gameLogic.stepCount === 1){
+			gameLogic.randomStep(count);
+			return;
+		}
+		if (gameLogic.checkPlayerStepInRow(count)){
+			return;
+		}
+		if (gameLogic.checkPlayerStepInCells(count)){
+			return;
+		}
+		gameLogic.checkPlayerStepInDiagonal(count);
+
+	}
+
+	randomStep(count){
+		var i = Math.floor(Math.random() * (count - 1) );
+		var j = Math.floor(Math.random() * (count - 1) );
+		if (!gameLogic.board.rows[i].cells[j].textContent){
+			gameLogic.board.rows[i].cells[j].innerHTML = "o";
+			gameLogic.board.rows[i].cells[j].classList.add("o");
+		}
+		else {
+			i = Math.floor(Math.random() * (count - 1) );
+			j = Math.floor(Math.random() * (count - 1) );
+			gameLogic.computerStep(i,j);
+		}
+	}
+
+	checkPlayerStepInDiagonal(count){
+		var number_x_elements = 0;
+		for (i = 0; i <count; i++){
+			if (gameLogic.board.rows[i].cells[i].innerHTML === "x"){
+				number_x_elements++;
+			}
+			if (number_x_elements === 2){
+				for (i = 0; i <count; i++){
+					if (!gameLogic.board.rows[i].cells[i].textContent){
+						gameLogic.board.rows[i].cells[i].innerHTML = "o";
+						gameLogic.board.rows[i].cells[i].classList.add("o");
+						number_x_elements = 0;
+						return true;
+					}
+
+				}
+			}
+		}
+		var i = 0;
+		number_x_elements = 0;
+		for (var j = count - 1; j >= 0; j--){
+			if (gameLogic.board.rows[i].cells[j].innerHTML === "x"){
+				number_x_elements++;
+			}
+			if (number_x_elements === 2){
+				i = 0;
+				for (j = count - 1; j >=0; j--){
+					if (!gameLogic.board.rows[i].cells[j].textContent){
+						gameLogic.board.rows[i].cells[j].innerHTML = "o";
+						gameLogic.board.rows[i].cells[j].classList.add("o");
+						number_x_elements = 0;
+						return true;
+					}
+					i++;
+				}
+			}
+			i++;
+		}
+		return false;
+	}
+
+	checkPlayerStepInRow(count){
+		for(var i = 0; i < count; i++){
+			var number_x_elements = 0;
+			var number_o_elements = 0;
+			for(var j = 0; j < count; j++){
+				if(gameLogic.board.rows[i].cells[j].innerHTML === "x") {
+					number_x_elements++;
+				}
+				if(gameLogic.board.rows[i].cells[j].innerHTML === "o") {
+					number_o_elements++;
+				}
+				if (number_x_elements === 2){
+					for(var k = 0; k < count; k++){
+						if (!gameLogic.board.rows[i].cells[k].textContent){
+							gameLogic.board.rows[i].cells[k].innerHTML = "o";
+							gameLogic.board.rows[i].cells[k].classList.add("o");
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	checkPlayerStepInCells(count){
+		for(var i = 0; i < count; i++){
+			var number_x_elements = 0;
+			var number_o_elements = 0;
+			for(var j = 0; j < count; j++){
+				if(gameLogic.board.rows[j].cells[i].innerHTML === "x") {
+					number_x_elements++;
+				}
+				if(gameLogic.board.rows[j].cells[i].innerHTML === "o") {
+					number_o_elements++;
+				}
+				if (number_x_elements === 2){
+					for(var k = 0; k < count; k++){
+						if (!gameLogic.board.rows[k].cells[i].textContent){
+							gameLogic.board.rows[k].cells[i].innerHTML = "o";
+							gameLogic.board.rows[k].cells[i].classList.add("o");
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	changePlayer(){
+		gameLogic.player === "x" ? (gameLogic.player = "o") : (gameLogic.player = "x");
 	}
 
 	checkWin(){
 		var flag;
+		gameLogic.stepCount++;
 		var count = gameLogic.board.getElementsByTagName("tr").length;
 		for(var i = 0; i < count; i++){
 			var winRow = true,
@@ -32,10 +171,15 @@ class GameLogic{
 
 			if(winRow || winColumn || winLeftTop || winLeftBottom){
 				flag = true;
+				const winString = gameLogic.player === "o" ? "Вы проиграли" : "Вы победили";
+				alert(winString);
 				gameLogic.removeEvent();
-				break;
+				return winString;
 			}
 
+		}
+		if (gameLogic.stepCount >= 9){
+			return "Ничья";
 		}
 	}
 
@@ -50,7 +194,12 @@ class GameLogic{
 		var elements = gameLogic.board.getElementsByTagName("td");
 		for (var i = 0; i < elements.length; i++) {
 			elements[i].innerHTML = '';
+			elements[i].classList.remove("x");
+			elements[i].classList.remove("o");
+			elements[i].addEventListener('click',gameLogic.currentStep);
 		}
+		gameLogic.player = "x";
+		gameLogic.stepCount = 0;
 	}
 
 }
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,4 +4,4 @@ for (var i = 0; i < articles.length; i++) {
 	articles[i].addEventListener('click',gameLogic.currentStep);
 }
 const resetGame = document.getElementById("reset-game");
-resetGame.addEventListener('click',gameLogic.removeBoard)
+resetGame.addEventListener('click',gameLogic.removeBoard);
