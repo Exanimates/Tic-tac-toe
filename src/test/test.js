660afc20045f4b46c32bfcd924bf611e93506037
Igor <exanimatesr@gmail.com>
2018-11-23 18:50:37 +0700

    Added a test that checks the function of changing the player

--- src/test/test.js
+++ src/test/test.js
@@ -1,7 +1,22 @@
 import GameLogic from "../js/GameLogic.js";
+import SimpleGameObject from "../js/Players/SimpleGameObject.js";
+import Computer from "../js/Players/Computer.js";
 
 describe("Test game logic", function() {
     const gameLogic = new GameLogic();
+
+    it("Check function change player ", ()=> {
+      //Если передаем Computer, то должен вернутся SimpleGameObject
+      const computer = new Computer("o");
+
+      assert.equal(gameLogic.changePlayer(new SimpleGameObject("x")), computer instanceof Computer);
+      
+      //Если передаем SimpleGameObject, то должен вернутся Computer
+      const simpleGameObject = new SimpleGameObject("x");
+
+      assert.equal(gameLogic.changePlayer(new Computer("o")), simpleGameObject instanceof SimpleGameObject);
+    });
+
     it("Check 'x' win horizontal ", ()=> {
       assert.equal(gameLogic.checkWin([
         ["x","x","x"], 
