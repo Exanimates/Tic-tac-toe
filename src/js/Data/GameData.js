06dcdced733964baeb8bd6c50e4fc553062c7c75
Igor <exanimatesr@gmail.com>
2018-11-23 20:39:35 +0700

    Added module GameData

---
+++ src/js/Data/GameData.js
@@ -0,0 +1,7 @@
+var GameData = {
+	board: document.getElementById("board"),
+	compCountHtml: document.getElementById("computer-count"),
+	playrCountHtml: document.getElementById("player-count"),
+	whoFirstMove : true
+}
+export default GameData;
