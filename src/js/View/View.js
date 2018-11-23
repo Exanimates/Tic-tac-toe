3c9c87c617f614a87569095d96a1acdb6c063086
Igor <exanimatesr@gmail.com>
2018-11-23 16:39:14 +0700

    Added drawing logic

--- src/js/View/View.js
+++ src/js/View/View.js
@@ -1,7 +1,54 @@
 export default class View
 {
-     constructor(){
-         this.color = color;
-         this.icon = icon;
+     constructor(gameLogic)
+     {
+         this.gameLogic = gameLogic;
      }
+
+     // Добавление нового элемента в историю игр
+	createTable(lengthBoard)
+	{
+		var historyBlock = document.getElementById("history");
+		var elementOfHistory = document.createElement("table");
+		for (let i = 0; i < lengthBoard; i++)
+		{
+			const row = document.createElement("tr");
+			for(let j = 0; j < lengthBoard; j++)
+			{
+				var td = document.createElement("td");
+				td.innerHTML = this.board.rows[i].cells[j].innerHTML;
+				td.classList = this.board.rows[i].cells[j].classList;
+				row.appendChild(td);
+			}
+			elementOfHistory.appendChild(row);
+		}
+		historyBlock.appendChild(elementOfHistory);
+	}
+
+	// Удаления событий повешенных на ячейке игрового поля
+	removeEvent(board)
+	{
+		const elements = board.getElementsByTagName("td");
+		for (let i = 0; i < elements.length; i++)
+		{
+			var elClone = elements[i].cloneNode(true);
+			elements[i].parentNode.replaceChild(elClone, elements[i]);
+		}
+	}
+	// Очисткая игрового поля
+	clearBoard(board)
+	{
+		const elements = board.getElementsByTagName("td");
+		for (let i = 0; i < elements.length; i++)
+		{
+			elements[i].innerHTML = "";
+			elements[i].classList.remove("x");
+			elements[i].classList.remove("o");
+			elements[i].addEventListener("click",()=>{
+				this.gameLogic.currentStep(elements[i]);
+			});
+		}
+		this.player = "x";
+		this.stepCount = 0;
+	}
 }
