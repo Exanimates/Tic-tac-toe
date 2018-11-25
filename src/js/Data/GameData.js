0b18195a532399d95f1d3063dd927ef4a19e0b72
Igor <exanimatesr@gmail.com>
2018-11-26 04:06:10 +0700

    Removed script GameData.js

--- src/js/Data/GameData.js
+++
@@ -1,7 +0,0 @@
-var GameData = {
-	board: document.getElementById("board"),
-	compCountHtml: document.getElementById("computer-count"),
-	playrCountHtml: document.getElementById("player-count"),
-	whoFirstMove : true
-}
-export default GameData;
