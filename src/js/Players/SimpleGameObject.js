ac7f94a609ab2964c9fd1657088c231a03149f8d
Igor <exanimatesr@gmail.com>
2018-11-26 04:15:44 +0700

    Added function step for SimpleGameObject

--- src/js/Players/SimpleGameObject.js
+++ src/js/Players/SimpleGameObject.js
@@ -3,4 +3,10 @@ export default class SimpleGameObject
      constructor(icon){
          this.icon = icon;
      }
+
+     step(articles)
+     {
+        articles.innerText = this.icon;
+        articles.classList.add("x");
+     }
 }
