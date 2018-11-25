dc96c62f5b2577e02eb67d7067453c96275ae965
Igor <exanimatesr@gmail.com>
2018-11-26 04:19:25 +0700

    Refactor main logic

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,30 @@ import GameLogic from './Logic/GameLogic.js';
 const gameLogic = new GameLogic();
 
 //На каждый элемент игрового поля вешаем функцию хода
-var articles = document.getElementsByTagName("td");
+var articles = document.getElementById("board").getElementsByTagName("td");
 for (let i = 0; i < articles.length; i++) {
 	articles[i].addEventListener("click", ()=>{
 		gameLogic.currentStep(articles[i]);
 	});
 }
 
-// По нажатию на кнопку reset-game удаляем поле
+// По нажатию на кнопку reset-game очищаем игровое поле
 const resetGame = document.getElementById("reset-game");
 
 resetGame.addEventListener("click", ()=>{
-	gameLogic.restartGame();
+	View.clearBoard(document.getElementById("board"));
+	gameLogic.stepCount = 0;
+	for (let i = 0; i < articles.length; i++) {
+		articles[i].addEventListener("click", ()=>{
+			gameLogic.currentStep(articles[i]);
+		});
+	}
 });
+
 // Скрываем/Показываем блок с историей игр
 const showHistory = document.getElementById("buttonShowHistory");
 
-showHistory.addEventListener("click", function () {  
+showHistory.addEventListener("click", () => {  
 	const history = document.getElementById("history");
 	if (history.style.display === "")
 	{
