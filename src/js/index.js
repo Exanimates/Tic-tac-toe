08235e104abffbac30ea2ecc170f1c8580093d91
Igor <exanimatesr@gmail.com>
2018-11-26 20:27:14 +0700

    Updated function calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,24 @@
 const gameLogic = new GameLogic();
 
-//На каждый элемент игрового поля вешаем функцию хода
 var articles = document.getElementById("board").getElementsByTagName("td");
-for (let i = 0; i < articles.length; i++) {
-	articles[i].addEventListener("click", ()=>{
-		gameLogic.clickOnCell(articles[i]);
-	});
-}
 
-// По нажатию на кнопку reset-game очищаем игровое поле
-const resetGame = document.getElementById("reset-game");
+addEvents();
 
-resetGame.addEventListener("click", ()=>{
-	View.clearBoard(document.getElementById("board"));
-	gameLogic.stepCount = 0;
-	for (let i = 0; i < articles.length; i++) {
+function addEvents() {
+	for (let i = 0; i <articles.length; i++) {
 		articles[i].addEventListener("click", ()=>{
 			gameLogic.clickOnCell(articles[i]);
 		});
 	}
-});
+}
+ 
+function clearBoard() {
+	View.clearBoard(document.getElementById("board"));
+	gameLogic.stepCount = 0;
+	addEvents();
+}
 
-// Скрываем/Показываем блок с историей игр
-const showHistory = document.getElementById("buttonShowHistory");
-
-showHistory.addEventListener("click", () => {  
+function showHistory() {
 	const history = document.getElementById("history");
 	if (history.style.display === "")
 	{
@@ -32,4 +26,4 @@ showHistory.addEventListener("click", () => {
 	}else{
 		history.style.display = "";
 	}
-});
+}
