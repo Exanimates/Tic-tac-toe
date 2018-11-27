830aad906253048f402b981b7a9515a50d5bab9d
Igor <exanimatesr@gmail.com>
2018-11-27 23:11:47 +0700

    Deleted server js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"),
-        app=express();
-app.listen(8080);   
-app.use(express.static(__dirname + '/View'));
-app.use("/src",express.static(__dirname + '/src'));
-app.get("/",(req, res)=>{
-    res.sendFile("index.html");
-});
